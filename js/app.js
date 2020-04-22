923dcef3bf79800e5569c06b78274587eceb1644
Jiyun <baejiyun01@gmail.com>
2020-04-22 19:06:19 -0400

    Updating js file

--- js/app.js
+++ js/app.js
@@ -49,3 +49,88 @@ new Chart(document.getElementById('lineChart'), {
     ]
   }
 });
+
+var updatingChart = new Chart(document.getElementById('updatingDataChart'), {
+  type: 'horizontalBar',
+  data: {
+    labels: ['Gold', 'Silver', 'Bronze'],
+    datasets: [{
+      label: 'Number',
+      backgroundColor: ['#FFD700', '#C0C0C0', '#AA7A55'],
+      data: ['123', '75', '41']
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    title: {
+      display: true,
+      text: 'Medals by Women in CANADA'
+    }
+  }
+});
+
+// Adding data & label to existing chart
+function addData(chart, label, data, backgroundColor) {
+  chart.data.labels.push(label);
+  chart.data.datasets[0].data.push(data);
+  chart.data.datasets[0].backgroundColor.push(backgroundColor);
+  chart.update();
+}
+
+setTimeout(function () {
+  addData(updatingChart, 'Antarctica', 97, '#FF5733');
+}, 3000);
+
+// Removing data & label from existing chart
+function removeData(chart) {
+  chart.data.labels.pop();
+  chart.data.datasets[0].data.pop();
+
+  chart.update();
+}
+
+setTimeout(function () {
+  removeData(updatingChart);
+}, 3000);
+
+// update all data points on existing chart
+function updateAllData(chart, data) {
+  chart.data.datasets[0].data = data;
+  chart.update();
+}
+
+setTimeout(function() {
+  updateAllData(updatingChart, [5,2,6]);
+}, 3000);
+
+function updateConfig(chart, title) {
+  chart.options.title.text = title;
+  chart.update();
+}
+
+setTimeout(function() {
+  updateAllData(updatingChart, [5,2,6])
+  updateConfig(updatingChart, 'Medals by Women in JAPAN');
+}, 3000);
+
+function updateConfig(chart, data) {
+  chart.data.datasets[0].data = data;
+  chart.update();
+}
+
+setTimeout(function() {
+  updateAllData(updatingChart, [60, 112, 71]);
+}, 3000);
+
+function updateConfig(chart, title) {
+  chart.options.title.text = title;
+  chart.update();
+}
+
+setTimeout(function() {
+  updateConfig(updatingChart, 'Medals by women in USA');
+}, 3000);
+
+
